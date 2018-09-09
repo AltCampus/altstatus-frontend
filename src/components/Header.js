c8c7eab3dbeac442d2ccefa1aa61d7bbd33819ce
Suraj Kumar <122suraj0@gmail.com>
2018-09-09 16:01:53 +0530

    add header in dashboard

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+			<header class="header">
+				<div class="container">
+					<nav class="navbar">
+						<h1 class="nav__brand">Altstatus</h1>
+						<div class="nav">
+							<div class="notification-bell">
+								<img src="assets/notification.png" alt="" />
+								<div class="circle" />
+							</div>
+							<div class="nav__item user">
+								<img src="assets/shape.png" alt="" />
+							</div>
+						</div>
+					</nav>
+				</div>
+			</header>
+    );
+  }
+}
+
+export default App;
