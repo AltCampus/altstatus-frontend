ac57dceed21d180c6a46d43b6053cd9ddede276c
nnnkit <iankit17@gmail.com>
2018-09-09 17:12:46 +0530

    add login and signup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import Router from './Router'
+import './style/index.scss';
 
 const store = createStore(todoApp)
 
